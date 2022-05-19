d7d1bf86be0cafbaf0a360dce4450b48063f14ea
Bermudez <732562@student.dusd.net>
2022-05-18 20:01:57 -0700

    Movement & Sprites Added
    Stellar has basic movement.

--- Stellar/Assets/Scripts/StellarController.cs
+++ Stellar/Assets/Scripts/StellarController.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class StellarController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 3.0f;
     void Start()
     {
-        
     }
-
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + 3.0f * horizontal * speed * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * speed * Time.deltaTime;
         transform.position = position;
     }
 }
+
