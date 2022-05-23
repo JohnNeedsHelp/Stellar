2fb41e11f66698cc2110127a2cab1325b44afc55
Bermudez <732562@student.dusd.net>
2022-05-22 22:44:06 -0700

    Start of Stellar Animations

--- Stellar/Assets/Scripts/StellarController.cs
+++ Stellar/Assets/Scripts/StellarController.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class StellarController : MonoBehaviour
 {
     public float speed = 3.0f;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     void Start()
     {
+        animator = GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
@@ -14,6 +21,18 @@ public class StellarController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         position.x = position.x + 3.0f * horizontal * speed * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * speed * Time.deltaTime;
         transform.position = position;
