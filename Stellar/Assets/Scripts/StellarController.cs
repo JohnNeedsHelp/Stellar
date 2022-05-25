32116dc784b4fa65239b5b1cba8dbb50dc84bb44
Bermudez <732562@student.dusd.net>
2022-05-24 17:07:39 -0700

    Working on the camera movement
    need the rest of the lvl to be filled out and made to continue.

--- Stellar/Assets/Scripts/StellarController.cs
+++ Stellar/Assets/Scripts/StellarController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class StellarController : MonoBehaviour
 {
-    public float speed = 1.5f;
+    public float speed = 2.0f;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
